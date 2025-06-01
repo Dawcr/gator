4eb7fa1aa2a5846598ad5da9eb46f47cd906e78f
dawcr <dawcraz@gmail.com>
2025-06-01 09:54:27 +0200

    added login

--- main.go
+++ main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dawcr/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	cfg.Print()
 
-	if err = cfg.SetUser("daw"); err != nil {
-		log.Fatalf("error setting username: %v", err)
+	appState := &state{
+		cfg: &cfg,
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
-	cfg.Print()
+
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("usage: cli <command> [args...]")
+	}
+
+	cmd := command{
+		Name: os.Args[1],
+		Args: os.Args[2:],
+	}
+
+	if err := cmds.run(appState, cmd); err != nil {
+		log.Fatalf("error running command %v: %v", cmd.Name, err)
+	}
 }
